9d0402235660b4aeb8b3e5185f6e6fe2882a7414
Stuart Hallows <stuart.hallows@gmail.com>
2024-07-15 22:11:02 +0930

    Initial commit

---
+++ DataStructures.Library/DoublyLinkedList.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructures.Library;
+
+public class DoubleLinkedListNode<TNode>
+{
+    public DoubleLinkedListNode(
+        TNode value,
+        DoubleLinkedListNode<TNode> previous = null,
+        DoubleLinkedListNode<TNode> next = null)
+    {
+        Value = value;
+        Next = next;
+        Previous = previous;
+    }
+    
+    public DoubleLinkedListNode<TNode> Previous { get; set; }
+    public DoubleLinkedListNode<TNode> Next { get; set; }
+    public TNode Value { get; set; }
+}
+
+public class DoublyLinkedList
+{
+}
---
+++ DataStructures.Library/LinkedList.cs
@@ -0,0 +1,18 @@
+﻿namespace DataStructures.Library;
+
+public class SingleLinkedListNode<TNode>
+{
+    public SingleLinkedListNode(TNode value, SingleLinkedListNode<TNode> next = null)
+    {
+        Value = value;
+        Next = next;
+    }
+    
+    public SingleLinkedListNode<TNode> Next { get; set; }
+    public TNode Value { get; set; }
+}
+
+public class LinkedList
+{
+    
+}
