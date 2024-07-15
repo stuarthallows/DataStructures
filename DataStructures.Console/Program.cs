23d86c533177172d344dd5007bc764cd3a79c9da
Stuart Hallows <stuart.hallows@gmail.com>
2024-07-15 22:11:27 +0930

    Linked lists

--- DataStructures.Console/Program.cs
+++ DataStructures.Console/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DataStructures.Library;
+
 Console.WriteLine("Hello, World!");
+
+/*
+ * Linked lists -
+ *      singly linked list,
+ *      doubly linked list - supports reverse traversal
+ *      sorted linked list
+ * Can be used as the underlying data structure for other structures like stacks, queues
+ */
+
+
+
+var doublyLinkedList = new DoublyLinkedList<int>();
+for(var i = 0; i < 10; i++)
+{
+    doublyLinkedList.AddHead(i);
+}
